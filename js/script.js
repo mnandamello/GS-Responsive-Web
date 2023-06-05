e5b82ff3c09ffcfe8b5137244bca364cbc5ddbc7
mnandamello <mfernanda.nintendo@gmail.com>
2023-06-05 19:14:24 -0300

    introduzi algumas coisas no JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
-function contudo(){
-  alert("Mensagem Enviada com sucesso");
+function obrigatorio(){
+
+  var Nome = document.formContact.nome;
+  var Sobrenome = document.formContact.sobrenome;
+  var Nascimento = document.formContact.nascimento;
+  var EstadoCivil = document.formContact.estCivil;
+  var Tel = document.formContact.telefone;
+  var Cel = document.formContact.celular;
+  var Email = document.formContact.email;
+  var Assunto = document.formContact.assunto;
+  var Msg = document.formContact.msg;
+
+  var lista = [Nome, Sobrenome, Nascimento, EstadoCivil, Tel, Cel, Email, Assunto, Msg]
+
+  var feedback = document.getElementById("retorno");
+
+  for (var contador = 0; contador < 9; contador++){
+    if (lista[contador].value == ""){
+        lista[contador].style.border = "1px solid";
+        lista[contador].style.borderColor = "red";
+    }else{
+        lista[contador]
+        lista[contador].style.border = "none";
+    }
+  }
+
+  for (var contador = 0; contador < 9; contador++){
+    if (lista[contador].value == ""){
+        feedback.innerHTML = "Preencha os campos obrigatórios.";
+        feedback.style.fontSize = "20px";
+        return false;
+    }
+  }
+
+  alert("Sua mensagem foi enviada com sucesso!")
+  return true;
+
+
 }
+
